f9dfb8db62b43cdd7fa7f1c18b0accff278b147e
Anushka Srivastava <47708011+Anushri-glitch@users.noreply.github.com>
2023-08-26 03:48:26 -0700

    Create SUV.java

---
+++ src/Question-2/SUV.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SUV {
+
+    public ArrayList<String> sCodes(ArrayList<String> arr) {
+        ArrayList<String> newList = new ArrayList<>();
+
+        for (String num : arr) {
+            if (num.equalsIgnoreCase("Basic servicing")) {
+                newList.add("BS01");
+            } else if (num.equalsIgnoreCase("Engine Fixing")) {
+                newList.add("EF01");
+            } else if (num.equalsIgnoreCase("Clutch Fixing")) {
+                newList.add("CF01");
+            } else if (num.equalsIgnoreCase("Brake Fixing")) {
+                newList.add("BF01");
+            } else if (num.equalsIgnoreCase("Gear Fixing")) {
+                newList.add("GF01");
+            }
+        }
+        return newList;
+    }
+
+    public Integer price(String str){
+        if(str.equalsIgnoreCase("Basic servicing")){
+            return 5000;
+        }
+        else if(str.equalsIgnoreCase("Engine Fixing")){
+            return 10000;
+        }
+        else if(str.equalsIgnoreCase("Clutch Fixing")){
+            return 6000;
+        }
+        else if(str.equalsIgnoreCase("Brake Fixing")){
+            return 2500;
+        }
+        else if(str.equalsIgnoreCase("Gear Fixing")){
+            return 8000;
+        }
+        return 0;
+    }
+
+    public HashMap<String,Integer> SuvBill(ArrayList<String> services) {
+
+        HashMap<String, Integer> bill = new HashMap<>();
+
+        for (String str : services) {
+            if (str.equalsIgnoreCase("Basic Servicing")) {
+                bill.put(str, price(str));
+            } else if (str.equalsIgnoreCase("Engine Fixing")) {
+                bill.put(str, price(str));
+            } else if (str.equalsIgnoreCase("Clutch Fixing")) {
+                bill.put(str, price(str));
+            } else if (str.equalsIgnoreCase("Brake Fixing")) {
+                bill.put(str, price(str));
+            } else if (str.equalsIgnoreCase("Gear Fixing")) {
+                bill.put(str, price(str));
+            }
+        }
+        return bill;
+    }
+}
